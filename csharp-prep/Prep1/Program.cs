a9bba01038492d84e5262af15caf74cb0ec949bc
professordavidx <gom22026@byui.edu>
2023-09-16 22:14:49 -0500

    Added changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        {   // Ask the user for their name.
+            Console.WriteLine("What is your first name?");
+            string first_name = Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            string last_name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last_name},{first_name} {last_name}");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+        int guessCount = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter magic number: ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guess == magicNumber)
+            {
+                Console.Write($"You guessed it in {guessCount} attempts.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("higher");
+            }
+        }
+
+
     }
 }
