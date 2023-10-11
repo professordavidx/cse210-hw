8dd22553dd7293c7ad5a6dd3ef84cc03e826e494
professordavidx <gom22026@byui.edu>
2023-10-11 11:13:49 -0500

    added changes

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning05
+{
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(string color, double radius) : base (color)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
